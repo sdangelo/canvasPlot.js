daf0ad35670d252e7d67e3a4abc0256869334226
Stefano D'Angelo <zanga.mail@gmail.com>
2015-03-11 17:57:14 +0100

    usual bad commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # canvasPlot.js
 Minimal HTML5 Canvas/JavaScript data plotting library.
 
-I am too lazy to explain hows and whys, just take a look at the example file index.html.
+I am too lazy to explain hows and whys, just take a look at the example file plot.html.
--- canvasPlot.js
+++ canvasPlot.js
@@ -20,11 +20,9 @@ var canvasPlot = {
 		vertical:	2
 	},
 
-	area: {
-		x:	0,
-		y:	0,
-		width:	0,
-		height: 0
+	point: {
+		x:	NaN,
+		y:	NaN
 	},
 
 	range: {
@@ -36,20 +34,73 @@ var canvasPlot = {
 		x:	null,
 		y:	null,
 
+		init: function (x, y) {
+			this.x = x ? x : [];
+			this.y = y ? y : [];
+		}
 	},
 
-	mapLinear: function (area, xrange, yrange, x, y) {
-		var kx1 = area.width / (xrange.max - xrange.min);
-		var kx0 = area.x - kx1 * xrange.min;
-		var ky1 = -area.height / (yrange.max - yrange.min);
-		var ky0 = area.y + area.height - ky1 * yrange.min;
+	area: {
+		p:	null,
+		width:	NaN,
+		height: NaN,
 
-		var ret	= {x: new Array(x.length), y: new Array(y.length)};
-		for (var i = 0; i < x.length; i++) {
-			ret.x[i] = kx0 + kx1 * x[i];
-			ret.y[i] = ky0 + ky1 * y[i];
+		p2:	null,
+
+		init: function (p) {
+			this.p = p ? p : Object.create(this.p);
+			this.p2 = Object.create(this.p2);
+		},
+
+		update: function () {
+			this.p2.x = this.p.x + this.width;
+			this.p2.y = this.p.y + this.height;
+		},
+
+		contains: function (x, y) {
+			return x >= this.p.x && x <= this.p2.x
+			       && y >= this.p.y && y <= this.p2.y;
 		}
-		return ret;
+	},
+
+	map: {
+		area:		null,
+		xRange:		null,
+		yRange:		null,
+
+		tmpPoint:	null,
+
+		init: function (area, xRange, yRange) {
+			if (area)
+				this.area = area;
+			else {
+				this.area = Object.create(this.area);
+				this.area.init(null);
+			}
+			this.xRange = xRange ? xRange
+					     : Object.create(this.xRange);
+			this.yRange = yRange ? yRange
+					     : Object.create(this.yRange);
+			this.tmpPoint = Object.create(this.area.p);
+		},
+
+		update: function () {
+		},
+
+		mapPoint: function (x, y, m) {
+			m.x = NaN;
+			m.y = NaN;
+		},
+
+		mapPoints: function (x, y, mx, my, xFirst, yFirst,
+				     mxFirst, myFirst, count) {
+			for (var i = 0; i < count; i++) {
+				this.mapPoint(x[xFirst + i], y[yFirst + i],
+					      this.tmpPoint);
+				mx[mxFirst + i] = this.tmpPoint.x;
+				my[myFirst + i] = this.tmpPoint.y;
+			}
+		}
 	},
 
 	frame: {
@@ -58,132 +109,191 @@ var canvasPlot = {
 		borderWidth:	2,
 		borderStyle:	"#000",
 
-		draw: function (ctx) {
-			if (!this.area || this.area.width == 0
-			    || this.area.height == 0)
-				return;
+		strokeArea:	null,
+		innerArea:	null,
 
+		init: function (area) {
+			if (area)
+				this.area = area;
+			else {
+				this.area = Object.create(this.area);
+				this.area.init(null);
+			}
+			this.strokeArea = Object.create(this.strokeArea);
+			this.strokeArea.init(null);
+			this.innerArea = Object.create(this.innerArea);
+			this.innerArea.init(null);
+		},
+
+		update: function () {
+			var bwh = 0.5 * this.borderWidth;
+			var bw2 = this.borderWidth + this.borderWidth;
+
+			this.strokeArea.p.x = this.area.p.x + bwh;
+			this.strokeArea.p.y = this.area.p.y + bwh;
+			this.strokeArea.width =
+				this.area.width - this.borderWidth;
+			this.strokeArea.height =
+				this.area.height - this.borderWidth;
+			this.strokeArea.update();
+
+			this.innerArea.p.x = this.area.p.x + this.borderWidth;
+			this.innerArea.p.y = this.area.p.y + this.borderWidth;
+			this.innerArea.width = this.area.width - bw2;
+			this.innerArea.height = this.area.height - bw2;
+			this.innerArea.update();
+		},
+
+		draw: function (ctx) {
 			ctx.save();
+
 			ctx.lineWidth = this.borderWidth;
 			ctx.strokeStyle = this.borderStyle;
 
-			ctx.strokeRect(this.area.x + 0.5 * this.borderWidth,
-				       this.area.y + 0.5 * this.borderWidth,
-				       this.area.width - this.borderWidth,
-				       this.area.height - this.borderWidth);
+			ctx.strokeRect(this.strokeArea.p.x, this.strokeArea.p.y,
+				       this.strokeArea.width,
+				       this.strokeArea.height);
 
 			ctx.restore();
-		},
-
-		getInnerArea: function () {
-			var area = Object.create(this.area);
-			area.x = this.area.x + this.borderWidth;
-			area.y = this.area.y + this.borderWidth;
-			area.width = this.area.width - this.borderWidth
-				     - this.borderWidth;
-			area.height = this.area.height - this.borderWidth
-				      - this.borderWidth;
-			return area;
 		}
 	},
 
 	tic: {
 		value:		NaN,
-		direction:	null,
+		direction:	NaN,
 
 		lineWidth:	1,
 		lineStyle:	"#ccc",
 
-		draw: function (ctx, area, map, xrange, yrange) {
-			var p = null;
-			if (this.direction == 1  // direction.horizontal
-			    && this.value >= yrange.min
-			    && this.value <= yrange.max)
-				var p = map(area, xrange, yrange,
-					    [xrange.min, xrange.max],
-					    [this.value, this.value]);
-			else if (this.direction == 2  // direction.vertical
-				   && this.value >= xrange.min
-				   && this.value <= xrange.max)
-				var p = map(area, xrange, yrange,
-					    [this.value, this.value],
-					    [yrange.min, yrange.max]);
+		toDraw:		false,
+		p1:		null,
+		p2:		null,
 
-			if (p) {
-				ctx.save();
+		init: function () {
+			this.p1 = Object.create(this.p1);
+			this.p2 = Object.create(this.p2);
+		},
 
-				ctx.lineWidth = this.lineWidth;
-				ctx.strokeStyle = this.lineStyle;
-				ctx.lineCap = "square";
-
-				ctx.rect(area.x, area.y,
-					 area.width, area.height);
-				ctx.clip();
-
-				ctx.beginPath();
-				ctx.moveTo(p.x[0], p.y[0]);
-				ctx.lineTo(p.x[1], p.y[1]);
-				ctx.stroke();
-
-				ctx.restore();
+		update: function (map) {
+			if (this.direction == 1  /* direction.horizontal */) {
+				if (this.value < map.yRange.min
+				    || this.value > map.yRange.max) {
+					this.toDraw = false;
+					return;
+				}
+				map.mapPoint(map.xRange.min, this.value,
+					     this.p1);
+				map.mapPoint(map.xRange.max, this.value,
+					     this.p2);
+				this.toDraw = true;
+			} else /* direction.vertical */ {
+				if (this.value < map.xRange.min
+				    || this.value > map.xRange.max) {
+					this.toDraw = false;
+					return;
+				}
+				map.mapPoint(this.value, map.yRange.min,
+					     this.p1);
+				map.mapPoint(this.value, map.yRange.max,
+					     this.p2);
+				this.toDraw = true;
 			}
+		},
+
+		draw: function (ctx, map) {
+			if (!this.toDraw)
+				return;
+
+			ctx.save();
+
+			ctx.lineWidth = this.lineWidth;
+			ctx.strokeStyle = this.lineStyle;
+			ctx.lineCap = "square";
+
+			ctx.rect(map.area.p.x, map.area.p.y,
+				 map.area.width, map.area.height);
+			ctx.clip();
+
+			ctx.beginPath();
+			ctx.moveTo(this.p1.x, this.p1.y);
+			ctx.lineTo(this.p2.x, this.p2.y);
+			ctx.stroke();
+
+			ctx.restore();
 		}
 	},
 
 	grid: {
-		tics:		null,
+		tics:	null,
 
-		draw: function (ctx, area, map, xrange, yrange) {
-			if (!this.tics)
-				return;
+		init: function (tics) {
+			if (tics) {
+				this.tics = tics;
+				for (var i = 0; i < this.tics.length; i++)
+					this.tics[i].init();
+			} else
+				this.tics = [];
+		},
 
+		update: function (map) {
 			for (var i = 0; i < this.tics.length; i++)
-				this.tics[i].draw(ctx, area, map,
-						  xrange, yrange);
+				this.tics[i].update(map);
+		},
+
+		draw: function (ctx, map) {
+			for (var i = 0; i < this.tics.length; i++)
+				this.tics[i].draw(ctx, map);
 		},
 	},
 
 	curve: {
 		samples:	null,
+		mSamples:	null,
 
 		lineWidth:	2,
 		lineStyle:	"#0f0",
 
 		pointRadius:	4,
 
-		draw: function (ctx, area, map, xrange, yrange) {
-			if (!this.samples || this.samples.length == 0)
-				return;
+		eAngle:		Math.PI + Math.PI,
 
-			var p = map(area, xrange, yrange,
-				    this.samples.x, this.samples.y);
+		init: function (samples, mSamples) {
+			this.samples = samples ? samples
+					       : Object.create(this.samples);
+			this.mSamples = mSamples ? mSamples
+					       : Object.create(this.samples);
+			if (samples && !mSamples) {
+				this.mSamples.init(Array(this.samples.x.length),
+					Array(this.samples.y.length));
+			} else if (!samples && mSamples) {
+				this.samples.init(Array(this.mSamples.x.length),
+					Array(this.mSamples.y.length));
+			} else {
+				this.samples.init(null, null);
+				this.mSamples.init(null, null);
+			}
+		},
 
-			var exists = function(x, y) {
-				return isFinite(x) && isFinite(y);
-			};
+		updatePart: function (map, first, count) {
+			map.mapPoints(this.samples.x, this.samples.y,
+				      this.mSamples.x, this.mSamples.y,
+				      first, first, first, first, count);
+		},
 
-			var areax2 = area.x + area.width;
-			var areay2 = area.y + area.height;
-			var inRange = function(x, y) {
-				return x >= area.x && x <= areax2
-				       && y >= area.y && y <= areay2;
-			};
+		update: function (map) {
+			this.updatePart(map, 0, this.samples.x.length);
+		},
 
-			var radius = this.pointRadius;
-			var eAngle = Math.PI + Math.PI;
-			var drawPoint = function(x, y) {
-				ctx.beginPath();
-				ctx.arc(x, y, radius, 0.0, eAngle);
-				ctx.fill();
-			};
-
+		drawPart: function (ctx, map, first, count) {
 			ctx.save();
+
 			ctx.lineWidth = this.lineWidth;
 			ctx.strokeStyle = this.lineStyle;
 			ctx.fillStyle = this.lineStyle;
 			ctx.lineJoin = "round";
 
-			ctx.rect(area.x, area.y, area.width, area.height);
+			ctx.rect(map.area.p.x, map.area.p.y,
+				 map.area.width, map.area.height);
 			ctx.clip();
 
 			var xPrev = NaN;
@@ -191,11 +301,11 @@ var canvasPlot = {
 			var exPrev = false;
 			var irPrev = false;
 			var pathBegun = false;
-			for (var i = 0; i < p.x.length; i++) {
-				var x = p.x[i];
-				var y = p.y[i];
-				var ex = exists(x, y);
-				var ir = inRange(x, y);
+			for (var i = first; i < count; i++) {
+				var x = this.mSamples.x[i];
+				var y = this.mSamples.y[i];
+				var ex = isFinite(x) && isFinite(y);
+				var ir = map.area.contains(x, y);
 
 				if (pathBegun) {
 					if (exPrev)
@@ -210,9 +320,9 @@ var canvasPlot = {
 						ctx.beginPath();
 						ctx.moveTo(xPrev, yPrev);
 						pathBegun = true;
-					} else {
-						drawPoint(xPrev, yPrev);
-					}
+					} else
+						this.drawPoint(ctx,
+							       xPrev, yPrev);
 				} else if (exPrev && ir) {
 					ctx.beginPath();
 					ctx.moveTo(xPrev, yPrev);
@@ -229,38 +339,93 @@ var canvasPlot = {
 					ctx.lineTo(xPrev, yPrev);
 				ctx.stroke();
 			} else if (irPrev)
-				drawPoint(xPrev, yPrev);
+				this.drawPoint(ctx, xPrev, yPrev);
 
 			ctx.restore();
+		},
+
+		draw: function (ctx, map) {
+			this.drawPart(ctx, map, 0, this.samples.x.length);
+		},
+
+		drawPoint: function (ctx, x, y) {
+			ctx.beginPath();
+			ctx.arc(x, y, this.pointRadius, 0.0, this.eAngle);
+			ctx.fill();
 		}
 	},
 
-	ctx:	null,
+	plot: {
+		map:	null,
+		frame:	null,
+		grid:	null,
+		curves:	null,
 
-	xrange: null,
-	yrange: null,
+		init: function (map, frame, grid, curves) {
+			if (frame)
+				this.frame = frame;
+			else {
+				this.frame = Object.create(this.frame);
+				this.frame.init(null);
+			}
 
-	curves:	null,
+			if (map)
+				this.map = map;
+			else {
+				this.map = Object.create(this.map);
+				this.map.init(this.frame.innerArea, null, null);
+			}
 
-	map:	null,
+			if (grid)
+				this.grid = grid;
+			else {
+				this.grid = Object.create(this.grid);
+				this.grid.init(null);
+			}
 
-	draw: function () {
-		this.ctx.clearRect(this.area.x, this.area.y,
-				   this.area.width, this.area.height);
-		var area = this.frame.getInnerArea();
+			this.curves = curves ? curves : [];
+		},
 
-		if (this.map != null) {
-			this.grid.draw(this.ctx, area, this.map,
-				       this.xrange, this.yrange);
-
-			if (this.curves != null)
+		update: function () {
+			if (this.grid)
+				this.grid.update(this.map);
+			if (this.curves)
 				for (var i = 0; i < this.curves.length; i++)
-					this.curves[i].draw(this.ctx, area,
-							    this.map,
-							    this.xrange,
-							    this.yrange);
-		}
+					this.curves[i].update(this.map);
+			if (this.frame)
+				this.frame.update();
+		},
 
-		this.frame.draw(this.ctx);
+		draw: function (ctx) {
+			if (this.grid)
+				this.grid.draw(ctx, this.map);
+			if (this.curves)
+				for (var i = 0; i < this.curves.length; i++)
+					this.curves[i].draw(ctx, this.map);
+			if (this.frame)
+				this.frame.draw(ctx);
+		}
 	}
 };
+
+canvasPlot.area.p = Object.create(canvasPlot.point);
+canvasPlot.area.p2 = Object.create(canvasPlot.point);
+
+canvasPlot.map.area = Object.create(canvasPlot.area);
+canvasPlot.map.xRange = Object.create(canvasPlot.range);
+canvasPlot.map.yRange = Object.create(canvasPlot.range);
+canvasPlot.map.tmpPoint = Object.create(canvasPlot.point);
+
+canvasPlot.tic.p1 = Object.create(canvasPlot.point);
+canvasPlot.tic.p2 = Object.create(canvasPlot.point);
+
+canvasPlot.frame.area = Object.create(canvasPlot.area);
+canvasPlot.frame.strokeArea = Object.create(canvasPlot.area);
+canvasPlot.frame.innerArea = Object.create(canvasPlot.area);
+
+canvasPlot.curve.samples = Object.create(canvasPlot.samples);
+canvasPlot.curve.mSamples = Object.create(canvasPlot.samples);
+
+canvasPlot.plot.map = Object.create(canvasPlot.map);
+canvasPlot.plot.frame = Object.create(canvasPlot.frame);
+canvasPlot.plot.grid = Object.create(canvasPlot.grid);
